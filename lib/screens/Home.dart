1f782c070c1e074620d4ca86975ae04d76c9d710
Sergio Steiner <serjo@serjo.live>
2022-10-05 20:09:55 -0300

    API consume added

---
+++ lib/screens/Home.dart
@@ -0,0 +1,184 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:store_app/consts/global_colors.dart';
+import 'package:store_app/screens/categories_screen.dart';
+import 'package:store_app/screens/feeds_screen.dart';
+import 'package:store_app/screens/users_screen.dart';
+import 'package:store_app/services/api_handler.dart';
+
+import '../models/products_model.dart';
+import '../widgets/appbar_icons.dart';
+import '../widgets/feeds_grid.dart';
+import '../widgets/feeds_widget.dart';
+import '../widgets/sale_widget.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late TextEditingController _textEditingController;
+
+  // List<ProductsModel> productsList = [];
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            // elevation: 4,
+            title: const Text('Cool Store'),
+            leading: AppBarIcons(
+              function: () {
+                Navigator.push(
+                  context,
+                  PageTransition(
+                    type: PageTransitionType.fade,
+                    child: const CategoriesScreen(),
+                  ),
+                );
+              },
+              icon: IconlyBold.category,
+            ),
+            actions: [
+              AppBarIcons(
+                function: () {
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.fade,
+                      child: const UsersScreen(),
+                    ),
+                  );
+                },
+                icon: IconlyBold.user3,
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 18,
+                ),
+                TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: "Search products",
+                      filled: true,
+                      fillColor: Theme.of(context).cardColor,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).cardColor,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      suffixIcon: Icon(
+                        IconlyLight.search,
+                        color: lightIconsColor,
+                      )),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(children: [
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: Swiper(
+                          itemCount: 3,
+                          itemBuilder: (ctx, index) {
+                            return const SaleWidget();
+                          },
+                          autoplay: true,
+                          pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
+                              builder: DotSwiperPaginationBuilder(
+                                  color: Colors.white,
+                                  activeColor: Colors.red)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            const Text(
+                              "Latest Products",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            const Spacer(),
+                            AppBarIcons(
+                                function: () {
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                          type: PageTransitionType.fade,
+                                          child: const FeedsScreen()));
+                                },
+                                icon: IconlyBold.arrowRight2),
+                          ],
+                        ),
+                      ),
+                      FutureBuilder<List<ProductsModel>>(
+                        future: APIHandler.getAllProducts(limit: "3"),
+                        builder: ((context, snapshot) {
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.none:
+                            case ConnectionState.waiting:
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            case ConnectionState.active:
+                            case ConnectionState.done:
+                              if (snapshot.hasError) {
+                                return Text(
+                                    "An error occurred ${snapshot.error}");
+                              } else {
+                                return FeedsGridWidget(
+                                    productsList: snapshot.data!);
+                              }
+                          }
+                        }),
+                      )
+                    ]),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
